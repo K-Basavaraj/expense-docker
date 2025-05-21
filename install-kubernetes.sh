38e15e0a1a46b0fd9be173393f1e7f0e79212570
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-05-22 05:10:55 +0530

    added installing kubectl and eksctl script

---
+++ install-kubernetes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e  # Exit immediately on error
+set -o pipefail
+
+# Download kubectl
+echo "Downloading kubectl..."
+curl -LO https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+
+# Make kubectl executable
+chmod +x ./kubectl
+
+# Move kubectl to /usr/local/bin
+sudo mv ./kubectl /usr/local/bin/kubectl
+echo "kubectl installed at /usr/local/bin/kubectl"
+
+# Verify kubectl version
+kubectl version --client
+
+# Set platform details
+ARCH=amd64
+PLATFORM="$(uname -s)_$ARCH"
+
+# Download eksctl tar.gz
+echo "Downloading eksctl..."
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
+
+# Extract and move eksctl
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv eksctl /usr/local/bin/eksctl
+
+echo "eksctl installed at /usr/local/bin/eksctl"
+
+# Verify eksctl version
+eksctl version
+
+# Run AWS configure
+echo "Running AWS configure..."
+aws configure
